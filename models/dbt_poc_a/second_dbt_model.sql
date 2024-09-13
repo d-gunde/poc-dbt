b9e8f9bfa3262a5ce69e1166d1ba78c81774b8d9
d-gunde <deekshagunde2022@gmail.com>
2024-09-13 15:53:24 -0400

    renamed column

--- models/dbt_poc_a/second_dbt_model.sql
+++ models/dbt_poc_a/second_dbt_model.sql
@@ -1,8 +1,8 @@
 
 WITH  ref_data AS (
-    SELECT 1 AS id, 'Doe' AS second_name
+    SELECT 1 AS id, 'Doe' AS last_name
     UNION ALL 
-    SELECT 2 AS id, 'Smith' AS second_name
+    SELECT 2 AS id, 'Smith' AS last_name
 )
 
 SELECT * FROM ref_data
